2695e0ced4bbd135b2da58ba30e61ec9bd072aa4
Sanzhar Alimkhanov <orlovssky@protonmail.com>
2023-04-06 15:16:55 +0400

    dev ext

--- src/entities/dolphinData/lib/helpers/extractDolphinTokenData.ts
+++ src/entities/dolphinData/lib/helpers/extractDolphinTokenData.ts
@@ -3,22 +3,19 @@ import { IDolphinTokenData } from "../typings/dolphinToken";
 const extractDolphinTokenData = (token: string): IDolphinTokenData | null => {
   try {
     const decodedToken = window.atob(token);
+    const [host, authorization, type] = decodedToken.split("::");
 
-    if (decodedToken.includes("::")) {
-      const [host, authorization] = decodedToken.split("::");
-
-      if (host && authorization) {
-        return {
-          host,
-          authorization,
-          dolphinType: "server",
-        };
-      }
-    } else if (decodedToken.trim()) {
+    if (host && authorization && type === "cloud") {
       return {
-        host: "https://cloud.dolphin.tech/api/v1",
+        host,
         authorization: `Bearer ${decodedToken.trim()}`,
         dolphinType: "cloud",
+      };
+    } else if (host && authorization) {
+      return {
+        host,
+        authorization,
+        dolphinType: "server",
       };
     }
 
